c04119deaf2c4589aaf65f31a762d2c554648d0a
Gu <862711634@qqcom>
2019-12-05 16:53:55 +0800

    创建工程

---
+++ test-git/src/main/java/com/gzh/demo/GitTest.java
@@ -0,0 +1,12 @@
+package com.gzh.demo;
+
+/**
+ * @author Gu
+ * @emall 1721231246@qq.com
+ * @create 2019-12-05 16:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git");
+    }
+}
